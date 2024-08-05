46ff232327f22f975ade3e826f0ee0d659651651
Eldrige <peldrige8@gmail.com>
2024-08-05 15:33:41 +0200

    add mozilla as user agent

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """__summary__
 - Write a Python script that fetches https://alu-intranet.hbtn.io/status
-- using the urllib package.
+- using the urllib package and a Mozilla user agent.
 """
 import urllib.request
 
-
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    # Create a request object with the Mozilla user agent
+    request = urllib.request.Request('https://intranet.hbtn.io/status')
+    request.add_header(
+        'User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0')
+
+    with urllib.request.urlopen(request) as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
